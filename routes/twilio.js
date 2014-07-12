642e6e646e7c86f5f84cd1036b27dac453454191
Morgante Pell <morgante.pell@morgante.net>
2014-07-12 18:58:27 -0400

    twilio to the max

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,8 +1,39 @@
 var twilio = require('twilio');
 var client = twilio(process.env['TWILIO_ACCOUNT_SID'], process.env['TWILIO_AUTH_TOKEN']);
 
+var request = require('request');
+var gju = require('geojson-utils');
+var fs = require('fs');
+
+var _ = require('underscore');
+
+var hoods = fs.readFileSync('./public/data/nyc.geojson');
+hoods = JSON.parse(hoods);
+hoods = hoods.features;
+
+function getPoint(address, cb) {
+	var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + process.env['GOOGLE_KEY'];
+
+	request(url, function(err, res, body) {
+		if (!err && res.statusCode == 200) {
+			var data = JSON.parse(body);
+			cb(null, data.results[0].geometry.location);
+		}
+	});
+}
+
+function getHood(point, cb) {
+	var hood = _.find(hoods, function(hood) {
+		var poly = hood.geometry;
+
+		return gju.pointInPolygon({"type":"Point","coordinates":point}, poly);
+	});
+
+	cb(null, hood);
+}
+
 exports.receive_sms = function(req, res, next) {
-	if( req.body.Body == undefined ) {
+	if( req.body.Body === undefined ) {
 		data = {
 			AccountSid: 'AC0a02a3f4c90ddfe838bfd56ca7a9b99d',
 			Body: '1742 1st ave',
@@ -27,10 +58,14 @@ exports.receive_sms = function(req, res, next) {
 
 	var address = data.Body + ' New York, NY';
 
-	res.setHeader('Content-Type', 'text/xml');
+	getPoint(address, function(err, data) {
+		getHood([data.lng, data.lat], function(err, hood) {
+			res.setHeader('Content-Type', 'text/xml');
 
-	var twiml = new twilio.TwimlResponse();
-	twiml.sms("I am a bob");
+			var twiml = new twilio.TwimlResponse();
+			twiml.sms(hood.properties.neighborhood + ", " + hood.properties.borough);
 
-	res.send(twiml.toString());
+			res.send(twiml.toString());
+		});
+	});	
 };
